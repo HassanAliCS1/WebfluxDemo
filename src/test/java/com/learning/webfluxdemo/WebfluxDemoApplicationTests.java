7b6cd4bfbd9c963f810cc4583156d517a3e19615
Hassan_ALi <hassan.ali@dailypay.com>
2023-09-25 14:09:14 +0100

    first commit

---
+++ src/test/java/com/learning/webfluxdemo/WebfluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.webfluxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
